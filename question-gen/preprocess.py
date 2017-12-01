9cd05023b97a2d274272cae285e08aa90c26caf7
ankeshanand <ankeshanand1994@gmail.com>
2017-12-01 18:54:51 -0500

    Update preprocess.py to use the new env interface

--- question-gen/preprocess.py
+++ question-gen/preprocess.py
@@ -1,37 +1,28 @@
 import json
 import os
 
-from home_platform.core import House
-from home_platform.rendering import Panda3dRenderWorld
-from home_platform.semantic import SuncgSemanticWorld, MaterialColorTable, MaterialTable
+from home_platform.env import BasicEnvironment
+from home_platform.semantic import MaterialColorTable, MaterialTable, SuncgSemantics
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "tests/data")
 TEST_SUNCG_DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "tests/data", "suncg")
 
 
-def compute_all_relationships(house):
+def compute_all_relationships(scene):
     """
     Computes relationships between all pairs of objects in the scene.
     """
 
-    house_objects = [x for x in house.objects]
-    room_objects = []
-
-    for room in house.rooms:
-        for obj in room.objects:
-            room_objects.append(obj)
-
+    objects = scene.getAllObjects()
     directions = ['east', 'west', 'north', 'south']
-    all_objects = house_objects + room_objects
     all_relationships = {x: [] for x in directions}
 
-    for i, obj1 in enumerate(all_objects):
+    for i, obj1 in enumerate(objects):
         related_objects = {x: [] for x in directions}
-        for j, obj2 in enumerate(all_objects):
+        for j, obj2 in enumerate(objects):
             if obj1 == obj2:
                 continue
-            diff = [obj1.location[k] - obj2.location[k] for k in [0, 1]]
-
+            diff = [obj1.getPos()[k] - obj2.getPos()[k] for k in [0, 1]]
             if diff[0] > 0:
                 related_objects['east'].append(j)
             else:
@@ -48,18 +39,21 @@ def compute_all_relationships(house):
     return all_relationships
 
 
-def set_object_properties(obj):
+def get_object_properties(obj, semantic_world):
     """
     Sets category, color and material properties of an object
     """
-    semantic_world = SuncgSemanticWorld(TEST_SUNCG_DATA_DIR)
 
-    obj.shape = semantic_world.categoryMapping.getCoarseGrainedCategoryForModelId(obj.modelId)
-    obj.size = "normal"
-    obj.color = MaterialColorTable.getBasicColorsFromObject(obj, mode='basic')[0]
-    obj.material = ' '.join(MaterialTable.getMaterialNameFromObject(obj))
+    properties = {}
 
-    return obj
+    semanticsNp = obj.find('**/semantics')
+    if not semanticsNp.isEmpty():
+        properties['size'] = semanticsNp.getTag('overall-size')
+        properties['shape'] = semanticsNp.getTag('coarse-category')
+        properties['color'] = semanticsNp.getTag('basic-colors')
+        properties['material'] = semanticsNp.getTag('materials')
+
+    return properties
 
 
 def generate_scene_metadata(house_id):
@@ -68,12 +62,8 @@ def generate_scene_metadata(house_id):
     :param house_id:
     :return:
     """
-    renderer = Panda3dRenderWorld(shadowing=False, showCeiling=False, mode='onscreen')
-    house = House.loadFromJson(
-        os.path.join(TEST_SUNCG_DATA_DIR, "house", house_id, "house.json"),
-        TEST_SUNCG_DATA_DIR)
-    renderer.addHouseToScene(house)
-
+    env = BasicEnvironment(houseId=house_id, suncgDatasetRoot=TEST_SUNCG_DATA_DIR)
+    scene = env.scene
     keys = ['shape', 'size', 'color', 'material']
 
     metadata = {
@@ -84,27 +74,20 @@ def generate_scene_metadata(house_id):
                 "image_index": 0,
                 "image_filename": "SUNCG_env_000000.png",
                 "objects": [],
-                "realationships": {}
+                "relationships": {}
             }
         ]
     }
 
-    for idx, obj in enumerate(house.objects):
-        obj = set_object_properties(obj)
-        house.objects[idx] = obj
-        metadata['scenes'][0]['objects'].append({k: v for k, v in vars(obj).iteritems() if k in keys})
 
-    for room in house.rooms:
-        for idx, obj in enumerate(room.objects):
-            obj = set_object_properties(obj)
-            room.objects[idx] = obj
-            metadata['scenes'][0]['objects'].append({k: v for k, v in vars(obj).iteritems() if k in keys})
+    semantic_world = SuncgSemantics(scene, TEST_SUNCG_DATA_DIR)
+    for idx, obj in enumerate(scene.getAllObjects()):
+        props = get_object_properties(obj, semantic_world)
+        metadata['scenes'][0]['objects'].append({k: v for k, v in props.iteritems() if k in keys})
 
-    metadata['scenes'][0]['relationships'] = compute_all_relationships(house)
-    renderer.destroy()
+    metadata['scenes'][0]['relationships'] = compute_all_relationships(env.scene)
 
     return metadata
-
 
 if __name__ == '__main__':
     house_id_list = ['0004d52d1aeeb8ae6de39d6bd993e992']
