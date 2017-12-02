ce585e97ee12fc42242a238976561af17edb08b0
Florian Golemo <fgolemo@gmail.com>
2017-12-01 23:07:56 -0500

    fixed shitty panda demo (made it Travis-compatible)

--- tests/experiments/panda-physics.py
+++ tests/experiments/panda-physics.py
@@ -1,9 +1,10 @@
-import direct.directbase.DirectStart
-from panda3d.core import Vec3
+from panda3d.core import Vec3, ClockObject
 from panda3d.bullet import BulletWorld
 from panda3d.bullet import BulletPlaneShape
 from panda3d.bullet import BulletRigidBodyNode
 from panda3d.bullet import BulletBoxShape
+from direct.showbase import ShowBase
+base = ShowBase.ShowBase()
 
 base.cam.setPos(10, -30, 20)
 base.cam.lookAt(0, 0, 5)
@@ -16,12 +17,12 @@ world.setGravity(Vec3(0, 0, -9.81))
 shape = BulletPlaneShape(Vec3(0, 0, 1), 1)
 node = BulletRigidBodyNode('Ground')
 node.addShape(shape)
-np = render.attachNewNode(node)
+np = base.render.attachNewNode(node)
 np.setPos(0, 0, -2)
 world.attachRigidBody(node)
 
 # Boxes
-model = loader.loadModel('models/box.egg')
+model = base.loader.loadModel('models/box.egg')
 model.setPos(-0.5, -0.5, -0.5)
 model.flattenLight()
 shape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
@@ -29,11 +30,12 @@ for i in range(10):
     node = BulletRigidBodyNode('Box')
     node.setMass(1.0)
     node.addShape(shape)
-    np = render.attachNewNode(node)
+    np = base.render.attachNewNode(node)
     np.setPos(0, 0, 2 + i * 2)
     world.attachRigidBody(node)
     model.copyTo(np)
 
+globalClock = ClockObject.getGlobalClock()
 
 # Update
 def update(task):
@@ -42,5 +44,5 @@ def update(task):
     return task.cont
 
 
-taskMgr.add(update, 'update')
-run()
+base.taskMgr.add(update, 'update')
+base.run()
