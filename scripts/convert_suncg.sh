1a46b6af5423bdf2065a485a39dff9fffb9c39d0
Vladislav Kurenkov <castielblak@gmail.com>
2018-02-04 16:59:04 +0300

    Update convert_suncg.sh to have more verbosity
    - Count total number of files to convert
    - Count number of EGG errors
    - Count number of BAM errors
    - Output current progress in format: number of processed files / total number of files
    - Output number of errors at the end or notify about success

--- scripts/convert_suncg.sh
+++ scripts/convert_suncg.sh
@@ -2,14 +2,27 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Counting
+num_seen=0
+num_egg_errors=0
+num_bam_errors=0
+
 # Temporarily setting the internal field seperator (IFS) to the newline character.
 IFS=$'\n';
 
 # Recursively loop through all OBJ files in the specified directory
 OBJ_DIRECTORY=$1
 has_err=0
-echo "Finding all objects to convert... (may take a moment)"
-for obj in $(find ${OBJ_DIRECTORY} -name '*.obj'); do
+
+echo "Finding all objects to convert... (be patient)"
+objects=($(find ${OBJ_DIRECTORY} -name '*.obj'))
+num_objects=${#objects[*]}
+echo "Found ${num_objects} objects..."
+printf "Start converting... (be patient)\n\n"
+
+for obj in ${objects[*]}; do
+	((++num_seen))
+	echo "Processing file ${num_seen}/${num_objects}"
 	echo "Processing OBJ file ${obj}"
 
 	INPUT_OBJ_FILE="${obj}"
@@ -28,19 +41,24 @@ for obj in $(find ${OBJ_DIRECTORY} -name '*.obj'); do
 	python ${DIR}/obj2egg.py --coordinate-system=y-up-right ${INPUT_OBJ_FILE}
 	if ! [ -f $OUTPUT_EGG_FILE ]; then
 		echo "Could not find output file ${OUTPUT_EGG_FILE}. An error probably occured during conversion."
-		has_err=1
+		((has_err=1))
+		((++num_egg_errors))
 	else
         egg2bam -ps rel -o ${OUTPUT_BAM_FILE} ${OUTPUT_EGG_FILE}
         if ! [ -f $OUTPUT_BAM_FILE ]; then
-            echo "Could not find output file ${OUTPUT_BAM_FILE}. An error probably occured during conversion."
-            has_err=1
+		echo "Could not find output file ${OUTPUT_BAM_FILE}. An error probably occured during conversion."
+            	((has_err=1))	
+		((++num_bam_errors))
         fi
     fi
 done
 
-echo 'All done.'
+printf 'All done.\n\n'
 
 if [[ has_err -eq 1 ]]; then
-    echo 'IMPORTANT! There were some errors. This happens sometimes. Please run this again.'
-    #TODO: actually count errors and display here the number of errors
+    echo "Failure! There were some errors. This happens sometimes. Please run this again."
+    echo "Number of egg errors: ${num_egg_errors}"
+    echo "Number of bam errors: ${num_bam_errors}"
+else
+    echo "Success! No errors occured."
 fi
